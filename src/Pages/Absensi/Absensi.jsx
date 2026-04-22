e74be22ab14c100a78993d757aa763a230454ed7
Ahmad Fathurrahman Ramdhani <ahmad@gmail.com>
2026-04-22 19:55:27 +0700

    Tabel Absensi

--- src/Pages/Absensi/Absensi.jsx
+++ src/Pages/Absensi/Absensi.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import "./Absensi.css";
 
 const Absensi = () => {
   return (
-    <div>
-       <h3>Absensi</h3>
-    </div>
-  )
-}
+    <div className="absensi-container">
+      <div className="absensi-header">
+        <h3>Absensi Mahasantri</h3>
+      </div>
 
-export default Absensi
+      {/* Tabel santri */}
+      <div className="table-mahasantri">
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Mahasantri</th>
+              <th>Hadir</th>
+              <th>Sakit</th>
+              <th>Izin</th>
+              <th>Alpa</th>
+              <th>Keterangan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Rebby Padang </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>✓</td>
+              <td>Malas dia suka molor dan budeq di bangunin</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Absensi;
